17fa11975d17bf8ead92b21e7bf1bf39697069ce
thoriitsheoga <thoriitsheoga@gmail.com>
2025-04-13 00:15:11 +0200

    final touches on the landing page

--- laboo-beauty-bar/src/Components/nailPhone.jsx
+++ laboo-beauty-bar/src/Components/nailPhone.jsx
@@ -4,31 +4,33 @@ import locationImage from "./assets/location.jpg";
 
 const AddNailPhoneComponent = () => {
   return (
-    <div className="landing-page">
-      <div className="main-container">
-        <div className="left-side">
-          <img src={nailImage} alt="mockup" className="side-image" />
-        </div>
-        <div className="right-side">
-          <div className="title-wrapper">
-            <h3 className="white-text">Welcome to my booking site:</h3>
-            <div className="info-wrapper">
-              <h1 className="title">Laboo</h1>
-              <h3 className="title-trailer">b e a u t y b a r</h3>
+    <>
+      <div className="landing-page">
+        <div className="main-container">
+          <div className="left-side">
+            <img src={nailImage} alt="mockup" className="side-image" />
+          </div>
+          <div className="right-side">
+            <div className="title-wrapper">
+              <h3 className="white-text">Welcome to my booking site:</h3>
+              <div className="info-wrapper">
+                <h1 className="title">Laboo</h1>
+                <h3 className="title-trailer">b e a u t y b a r</h3>
+              </div>
+              <h4 className="services">
+                Nails| Luxury Hair| Hair Installation | Wig Customization
+              </h4>
             </div>
-            <h4 className="services">
-              Nails| Luxury Hair| Hair Installation | Wig Customization
-            </h4>
           </div>
         </div>
+        <div className="shop-location">
+          <img src={locationImage} className="location-image" />
+          <a src="#" className="location-tag">
+            Mankweng ,Turf, UL Gate 2
+          </a>
+        </div>
       </div>
-      <div className="shop-location">
-        <img src={locationImage} className="location-image" />
-        <a src="#" className="location-tag">
-          Mankweng ,Turf, UL Gate 2{" "}
-        </a>
-      </div>
-    </div>
+    </>
   );
 };
 export default AddNailPhoneComponent;
