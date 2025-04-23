cfe7513b8bc28c6b3f7f9a9a8ad89e12c41a67d4
thoriitsheoga <thoriitsheoga@gmail.com>
2025-04-23 13:04:32 +0200

    further updating the website

--- laboo-beauty-bar/src/App.jsx
+++ laboo-beauty-bar/src/App.jsx
@@ -1,12 +1,13 @@
 import NavBar from "./Components/home-page";
 import AddNailPhoneComponent from "./Components/nailPhone";
+import AboutSection from "./Components/about";
 import "./fonts/fonts.css";
-
 function App() {
   return (
     <>
       <NavBar />
       <AddNailPhoneComponent />
+      <AboutSection />
     </>
   );
 }
---
+++ laboo-beauty-bar/src/Components/about.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import ProfilePic from "./assets/ProfilePic.jpg";
+
+function AboutSection() {
+  return (
+    <>
+      <div className="about-container">
+        <div className="about-left">
+          <img
+            src={ProfilePic}
+            alt="your nail tech"
+            className="profile-picture"
+          />
+        </div>
+        <div className="about-right">
+          <h1 className="about-title">Your new favourite NailTech</h1>
+          <p className="about-description">
+            <div>
+              Hello, I am Lethabo, a self-taught nail tech situated at Gate 2,
+              UL
+            </div>
+            <div>
+              I look forward to making your pinterest board nails come to life.
+            </div>
+            <div>
+              My goal is to ultimately leave you pleased with your new set.
+            </div>
+            <div>I can’t wait to meet you🤍</div>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default AboutSection;
